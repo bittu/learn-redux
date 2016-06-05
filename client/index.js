d75995438633f31fec0da5a8defa259982d8533d
Sandeep Vemula <bittu4u.4ever@gmail.com>
2016-06-06 00:31:08 +0530

    4 done

--- client/index.js
+++ client/index.js
@@ -2,4 +2,19 @@ import React from 'react';
 import { render } from 'react-dom';
 import css from './styles/style.styl';
 
-render(<h1>hi</h1>, document.getElementById('root'));
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
+
+const router = (
+	<Router history={browserHistory}>
+		<Route path='/' component={Main}>
+			<IndexRoute component={PhotoGrid}/>
+			<Route path='/view/:postId' component={Single} />
+		</Route>
+	</Router>
+)
+
+render(router, document.getElementById('root'));
